3a852e6a0bfb8dc9bb735a63632b58059fdeedbb
khuongbv <khuong.bui.263@gmail.com>
2021-12-20 21:02:09 +0700

    Discussion Rails App

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  rolify
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :discussions, dependent: :destroy
+  has_many :channels, through: :discussions
+  has_one_attached :avatar
+
+end
